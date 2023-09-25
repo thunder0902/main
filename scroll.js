ae7025b8c04522b4d967cdb83355187f228186fe
썬더 <76557909+thunder0902@users.noreply.github.com>
2023-09-25 18:20:14 +0900

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -90,16 +90,65 @@
 //   });
 // }
 // // //-------------------------------------------------------
-var screen = document.querySelectorAll("section")[0];
-var screen2 = document.querySelectorAll("section")[1];
-var screen3 = document.querySelectorAll("section")[2];
-var textA = document.querySelector(".texta");
-var textB = document.querySelector(".textb");
-var textA2 = document.querySelector(".texta2");
-var textB2 = document.querySelector(".textb2");
-var textA3 = document.querySelector(".texta3");
-var textB3 = document.querySelector(".textb3");
-if (window.innerWidth < 600) {
+
+function updateScroll() {
+  var screen = document.querySelectorAll("section")[0];
+  var screen2 = document.querySelectorAll("section")[1];
+  var screen3 = document.querySelectorAll("section")[2];
+  var textA = document.querySelector(".texta");
+  var textB = document.querySelector(".textb");
+  var textA2 = document.querySelector(".texta2");
+  var textB2 = document.querySelector(".textb2");
+  var textA3 = document.querySelector(".texta3");
+  var textB3 = document.querySelector(".textb3");
+
+  let scrollPosition = window.scrollY;
+  // Animation logic for larger screens
+  var opacityValue = 1 - scrollPosition / 1000;
+  var opacityValue2 = (scrollPosition - 1) / 1200;
+  var opacityValue3 = (scrollPosition - 1000) / 1000;
+
+  opacityValue = Math.max(0, Math.min(1, opacityValue));
+  opacityValue2 = Math.max(0, Math.min(1, opacityValue2));
+  opacityValue3 = Math.max(0, Math.min(1, opacityValue3));
+
+  screen.style.opacity = opacityValue;
+  screen2.style.opacity = opacityValue2;
+  screen3.style.opacity = opacityValue3;
+
+  // Handle text animations
+  if (scrollPosition < 700) {
+    textA.style.transform = "translateX(-22200px)";
+    textB.style.transform = "translateX(3000px)";
+  } else if (scrollPosition >= 700) {
+    textA.style.transform = "translateX(0px)";
+    textB.style.transform = "translateX(95px)";
+  }
+  if (scrollPosition < 1700) {
+    textA2.style.transform = "translateX(-22200px)";
+    textB2.style.transform = "translateX(3000px)";
+  } else if (scrollPosition >= 1700) {
+    textA2.style.transform = "translateX(0px)";
+    textB2.style.transform = "translateX(70px)";
+  }
+  if (scrollPosition < 2700) {
+    textA3.style.transform = "translateX(-22200px)";
+    textB3.style.transform = "translateX(3000px)";
+  } else if (scrollPosition >= 2700) {
+    textA3.style.transform = "translateX(0px)";
+    textB3.style.transform = "translateX(80px)";
+  }
+}
+
+// Add a single scroll event listener
+
+// Initial call to updateScroll to set initial state
+if (window.innerWidth > 600) {
+  updateScroll();
+  window.addEventListener("scroll", function () {
+    requestAnimationFrame(updateScroll);
+  });
+} else if (window.innerWidth < 700) {
   var newTexta = document.querySelector(".texta");
   var newTextb = document.querySelector(".textb");
   newTexta.innerHTML = "";
@@ -117,53 +166,3 @@ if (window.innerWidth < 600) {
   newTextb2.style.fontSize = "80px";
   newTextb3.style.fontSize = "80px";
 }
-
-function updateScroll() {
-  let scrollPosition = window.scrollY;
-  if (window.innerWidth > 600) {
-    // Animation logic for larger screens
-    var opacityValue = 1 - scrollPosition / 1000;
-    var opacityValue2 = (scrollPosition - 1) / 1200;
-    var opacityValue3 = (scrollPosition - 1000) / 1000;
-
-    opacityValue = Math.max(0, Math.min(1, opacityValue));
-    opacityValue2 = Math.max(0, Math.min(1, opacityValue2));
-    opacityValue3 = Math.max(0, Math.min(1, opacityValue3));
-
-    screen.style.opacity = opacityValue;
-    screen2.style.opacity = opacityValue2;
-    screen3.style.opacity = opacityValue3;
-
-    // Handle text animations
-    if (scrollPosition < 700) {
-      textA.style.transform = "translateX(-22200px)";
-      textB.style.transform = "translateX(3000px)";
-    } else if (scrollPosition >= 700) {
-      textA.style.transform = "translateX(0px)";
-      textB.style.transform = "translateX(100px)";
-    }
-    if (scrollPosition < 1700) {
-      textA2.style.transform = "translateX(-22200px)";
-      textB2.style.transform = "translateX(3000px)";
-    } else if (scrollPosition >= 1700) {
-      textA2.style.transform = "translateX(0px)";
-      textB2.style.transform = "translateX(80px)";
-    }
-    if (scrollPosition < 2700) {
-      textA3.style.transform = "translateX(-22200px)";
-      textB3.style.transform = "translateX(3000px)";
-    } else if (scrollPosition >= 2700) {
-      textA3.style.transform = "translateX(0px)";
-      textB3.style.transform = "translateX(90px)";
-    }
-  }
-}
-
-// Add a single scroll event listener
-
-// Initial call to updateScroll to set initial state
-updateScroll();
-
-window.addEventListener("scroll", function () {
-  requestAnimationFrame(updateScroll);
-});
